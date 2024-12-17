c876465cadb85235f2738bb00ac6c066d81e0b1f
최재혁 <wogur8531@naver.com>
2024-12-17 16:11:14 +0900

    중간점검

--- fruitshop/src/main/java/admin/admincontroller/ManagementController.java
+++ fruitshop/src/main/java/admin/admincontroller/ManagementController.java
@@ -14,38 +14,33 @@ import member.domain.MemberVO;
 public class ManagementController extends AbstractController {
 
 	private AdminDAO adao = new AdminDAO_imple();
-	
+
 	@Override
 	public void execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
-		
-		
-		HttpSession session = request.getSession(); 
-		
-		MemberVO loginuser = (MemberVO)(session.getAttribute("loginuser"));
-		
-		/*
-		 * if(null == loginuser) { System.out.println("비정상적인 접근입니다."); }else
-		 * if(loginuser.getRole() == 1){ System.out.println("비정상적인 접근입니다."); }else {
-		 */
+
+		HttpSession session = request.getSession();
+
+		MemberVO loginuser = (MemberVO) (session.getAttribute("loginuser"));
+
+	
+		if(null == loginuser) {
+			System.out.println("비정상적인 접근입니다.");
+		}else if(loginuser.getRole() == 1){ 
+			System.out.println("비정상적인 접근입니다."); 
+		}else {
 			String ctxPath = request.getContextPath();
-			
+
 			List<MemberVO> member_allList = adao.MemberSelectAll();
-			
+
 			request.setAttribute("member_allList", member_allList);
-			
+
 			request.setAttribute("adminpage_val", "admin_member_management");
-			
+
 			System.out.println(member_allList.size());
-			
+
 			super.setRedirect(false);
 			super.setViewPage("/WEB-INF/admin_page/admin_page.jsp");
-		//}
-		
-		
-		
-		
-		
-		
+		}
 		
 
 	}
